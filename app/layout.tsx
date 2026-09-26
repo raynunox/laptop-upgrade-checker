41f84b691d48560f342ca6a793489db221ebef4d
Hail Santa <gembarsur@outlook.com>
2026-09-26 13:55:24 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "../components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} bg-gray-50 text-gray-900 antialiased`}>
         {children}
+        <Footer />
       </body>
     </html>
   );
